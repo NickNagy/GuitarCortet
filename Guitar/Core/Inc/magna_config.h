eeea210dbb17456bf3ec55592aad0c734e71c081
Nick Nagy <nickvincentnagy@gmail.com>
2022-01-29 08:06:07 -0800

    code cleanup

--- Guitar/Core/Inc/magna_config.h
+++ Guitar/Core/Inc/magna_config.h
@@ -20,4 +20,63 @@
 #define THROW_ALLOC_EXCEPTIONS 1
 #define DEBUG_ADD_DIAL 1
 
+/* overload C++ new and delete operators to make them thread-safe */
+void* operator new (std::size_t count) {
+	void *p;
+#if USING_FREERTOS
+	if (uxTaskGetNumberOfTasks())
+		p = pvPortMalloc(count);
+	else
+		p = malloc(count);
+#if THROW_ALLOC_EXCEPTIONS
+		if (p == 0)
+			throw std::bad_alloc();
+#endif
+#else
+	p = malloc(count);
+#endif
+	return p;
+}
+
+void operator delete(void* ptr) noexcept {
+#if USING_FREERTOS
+	if (uxTaskGetNumberOfTasks())
+		vPortFree(ptr);
+	else
+		free(ptr);
+#else
+	free(ptr);
+#endif
+	ptr = NULL;
+}
+
+void *operator new[](std::size_t count) {
+	void *p;
+#if USING_FREERTOS
+	if (uxTaskGetNumberOfTasks())
+		p = pvPortMalloc(count);
+	else
+		p = malloc(count);
+#if THROW_ALLOC_EXCEPTIONS
+		if (p == 0)
+			throw std::bad_alloc();
+#endif
+#else
+	p = malloc(count);
+#endif
+	return p;
+}
+
+void operator delete[](void * ptr) noexcept {
+#if USING_FREERTOS
+	if (uxTaskGetNumberOfTasks())
+		vPortFree(ptr);
+	else
+		free(ptr);
+#else
+	free(ptr);
+#endif
+	ptr = NULL;
+}
+
 #endif /* INC_MAGNA_CONFIG_H_ */
